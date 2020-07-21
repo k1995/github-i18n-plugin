858a6726f2991eaeae3ddaa7e342cb489b8534f2
k1995 <k1995328@gmail.com>
2020-07-21 00:13:17 -0300

    add code

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ GitHub.com 国际化插件
 - 汉化GitHub菜单栏，标题，按钮等公共组件
 - 除基础组件汉化外，还支持对“项目描述”进行人机翻译
 
+## 安装
+
+本插件基于[油猴](https://www.baidu.com/s?wd=%E4%BB%80%E4%B9%88%E6%98%AF%E6%B2%B9%E7%8C%B4)开发，所以需要先安装油猴插件，支持的浏览器：Chrome, Microsoft Edge, Safari, Opera Next, 和 Firefox。
+
+油猴安装完毕后，请点击[此链接](https://greasyfork.org/zh-CN/scripts/407485-github-internationalization)进入安装。
+
+
 ## 效果截图
 
 ![screenshot1](./images/screenshot1.png)
-
-!screenshot1](./images/screenshot2.png)
 
 ## 贡献
 中文词条在`locales/zh-CN.js`目录中，欢迎补充完善。
---
+++ userscript.js
@@ -0,0 +1,132 @@
+// ==UserScript==
+// @name                GitHub Internationalization
+// @name:zh-CN          GitHub汉化插件
+// @namespace           https://github.com/k1995/github-i18n-plugin/
+// @version             0.1
+// @description         Translate GitHub.com
+// @description:zh-CN   GitHub汉化插件
+// @author              k1995
+// @match               https://github.com/*
+// @grant               GM_xmlhttpRequest
+// @require             https://raw.githubusercontent.com/k1995/github-i18n-plugin/master/locales/zh-CN.js
+// @require             http://code.jquery.com/jquery-2.1.1.min.js
+// @updateURL           https://raw.githubusercontent.com/k1995/github-i18n-plugin/master/userscript.js
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function translateElement(el) {
+    // Get the text field name
+    let k;
+    if(el.tagName === "INPUT") {
+      if (el.type === 'button' || el.type === 'submit') {
+        k = 'value';
+      } else {
+        k = 'placeholder';
+      }
+    } else {
+      k = 'data';
+    }
+
+    const txtSrc = el[k].trim();
+    const key = txtSrc.toLowerCase()
+        .replace(/\xa0/g, ' ') // replace '&nbsp;'
+        .replace(/\s{2,}/g, ' ');
+
+    if(txtSrc.startsWith("Sign")) {
+      console.log(key);
+      console.log(locales[key]);
+    }
+    if(locales[key]) {
+      el[k] = el[k].replace(txtSrc, locales[key])
+    }
+  }
+
+  function shoudTranslateEl(el) {
+    const blockIds = ["readme"];
+    const blockTags = ["CODE", "SCRIPT", "LINK", "IMG", "svg"];
+
+    return !(el.id && blockIds.includes(el.id))
+      && !(blockTags.includes(el.tagName));
+  }
+
+  function traverseElement(el) {
+    if(!shoudTranslateEl(el)) {
+      return
+    }
+
+    for(const child of el.childNodes) {
+      if(child.nodeType === Node.TEXT_NODE) {
+        translateElement(child);
+      }
+      else if(child.nodeType === Node.ELEMENT_NODE) {
+        if(child.tagName === "INPUT") {
+          translateElement(child);
+        } else {
+          traverseElement(child);
+        }
+      } else {
+        // pass
+      }
+    }
+  }
+
+  function watchUpdate() {
+    const m = window.MutationObserver || window.WebKitMutationObserver;
+    const observer = new m(function (mutations, observer) {
+      for(let mutationRecord of mutations) {
+        for(let node of mutationRecord.addedNodes) {
+          traverseElement(node);
+        }
+      }
+    });
+
+    observer.observe(document.body, {
+      subtree: true,
+      characterData: true,
+      childList: true,
+    });
+  }
+
+  // translate "about"
+  function translateDesc() {
+    $(".repository-content .f4").append("<br/>");
+    $(".repository-content .f4").append("<a id='translate-me' href='#' style='color:rgb(27, 149, 224);font-size: small'>翻译</a>");
+    $("#translate-me").click(function() {
+      // get description text
+      const desc = $(".repository-content .f4")
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text()
+        .trim();
+
+      if(!desc) {
+        return;
+      }
+
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: `https://www.githubs.cn/translate?q=`+ encodeURIComponent(desc),
+        onload: function(res) {
+          if (res.status === 200) {
+            $("#translate-me").hide();
+            // render result
+            const text = res.responseText;
+            $(".repository-content .f4").append("<span style='font-size: small'>由 <a href='color:rgb(27, 149, 224);'>GitHub中文社区</a> 翻译👇</span>");
+            $(".repository-content .f4").append("<br/>");
+            $(".repository-content .f4").append(text);
+          } else {
+            alert("翻译失败");
+          }
+        }
+      });
+    });
+  }
+
+  traverseElement(document.body);
+  translateDesc();
+  watchUpdate();
+})();
